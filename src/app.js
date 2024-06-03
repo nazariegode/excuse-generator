b4b5812549316e61b770d8dce56af37d2c2967ac
David Nazariego Ulacio <dnazariego@gmail.com>
2024-06-03 16:06:17 +0000

    generador excusas

--- src/app.js
+++ src/app.js
@@ -2,10 +2,37 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./assets/img/bg.png";
+import "./assets/img/d.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
+
+  function generateExcuse() {
+    let who = ["Mi perro", "Mi abuela", "El cartero", "Mi pájaro"];
+    let action = ["comió", "orinó", "chocó", "rompió"];
+    let what = ["mi tarea", "mi telefono", "el auto"];
+    let when = [
+      "antes de clases",
+      "cuando estaba durmiendo",
+      "mientras me ejercitaba",
+      "durante mi almuerzo",
+      "mientras estaba orando"
+    ];
+
+    let whoIndx = Math.floor(Math.random() * who.length);
+    let actionIndx = Math.floor(Math.random() * action.length);
+    let whatIndx = Math.floor(Math.random() * what.length);
+    let whenIndx = Math.floor(Math.random() * when.length);
+
+    return (
+      who[whoIndx] +
+      " " +
+      action[actionIndx] +
+      " " +
+      what[whatIndx] +
+      " " +
+      when[whenIndx]
+    );
+  }
 };
